5e64b1aefcf93a83f1de7da7e647248c8923de34
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-05-24 17:49:18 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = self.sort_word(word)
+
+    def sort_word(self, word):
+        return ''.join(sorted(word))
+
+    def match(self, anagram):
+        matches=[]
+        for candidate in anagram:
+            if self.sort_word(candidate) ==self.sorted_word:
+                matches.append(candidate)
+
+        return matches
